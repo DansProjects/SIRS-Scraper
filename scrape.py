767ebd0cad1ed2c453df864425f5fa6f5e8c6c6a
Dan Mo <dmo@princeton.edu>
2017-02-09 22:04:22 -0500

    init commit

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import configparser
+
+from Cas import Cas
+
+cp = configparser.RawConfigParser()
+configFilePath = 'config.txt'
+cp.read(configFilePath)
+
+cas_login_url = cp.get("cas-config", "cas_login_url")
+app_login_url = cp.get("cas-config", "app_login_url")
+netid = cp.get("cas-config", "netid")
+password = cp.get("cas-config", "password")
+
+cas = Cas(netid, password, cas_login_url, app_login_url)
+
+result = cas.cas_login()
+
+#with open("Output.txt", "w") as text_file:
+#    text_file.write(result)
