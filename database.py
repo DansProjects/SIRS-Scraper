c2eebaeb43574244268a5d3c99a1e3ee111d0e9d
Dan Mo <danmo1337@gmail.com>
2017-02-21 22:40:21 -0500

    Changes section to varchar

--- database.py
+++ database.py
@@ -17,7 +17,7 @@ class Course(Base):
     school = Column(Integer)
     department = Column(Integer)
     course = Column(Integer)
-    section = Column(Integer)
+    section = Column(String(25))
     instructor = Column(String(250))
     source = Column(String(250))
     enrollments = Column(Integer())
